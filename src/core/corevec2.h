445e21e858f55a01844a412d00d9b9e3530a0cb6
saom88n <eugeniymazepa@gmail.com>
2022-10-05 00:30:35 +0300

    corevec2.h

--- src/core/corevec2.h
+++ src/core/corevec2.h
@@ -5,16 +5,18 @@
 
 #include "../includes/coreincludes.h"
 
-Vec2 GetCenterOfScreen() {
-    return (Vec2) {(f32) SCREEN_WIDTH / 2, (f32) SCREEN_HEIGHT / 2};
-}
+namespace Engine {
+    
+    Vec2 GetCenterOfScreen() {
+        return (Vec2) {(f32) SCREEN_WIDTH / 2, (f32) SCREEN_HEIGHT / 2};
+    }
 
-f32 Vec2MoveX(Vec2 v, Vec2 t, f32 d) {
-    return v.x > 0 ? v.x-d : v.x ; 
-}
+    f32 Vec2MoveX(Vec2 v, Vec2 t, f32 d) {
+        return v.x > 0 ? v.x-d : v.x ; 
+    }
 
-f32 Vec2MoveY(Vec2 v, Vec2 t, f32 d) {
-    return v.y > 0 ? v.y-d : v.y ; 
+    f32 Vec2MoveY(Vec2 v, Vec2 t, f32 d) {
+        return v.y > 0 ? v.y-d : v.y ; 
+    }
 }
-
 #endif
